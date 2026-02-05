2bbb8b3548789aa0fde06541f8f3b219094db450
Bas <basarab.o@icloud.com>
2026-02-05 19:31:11 +0200

    editing url

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// const API_URL = process.env.NEXT_PUBLIC_API_URL + "/api";
-const API_URL =
-  (process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000") + "/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL + "/api";
 
 export const nextServer = axios.create({
   baseURL: API_URL,
